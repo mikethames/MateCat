e7a5a77f2d95a368f03ad43a2f862b8d14d9a988
riccio <riccio82@gmail.com>
2021-08-31 12:34:45 +0200

    Fix merge

--- public/js/cat_source/es6/components/projects/Dashboard.js
+++ public/js/cat_source/es6/components/projects/Dashboard.js
@@ -477,7 +477,6 @@ class Dashboard extends React.Component {
         this.state.teams &&
         this.state.showProjects ? (
           <ProjectsContainer
-            changeJobPasswordFn={API.JOB.changeJobPassword}
             downloadTranslationFn={this.downloadTranslation}
             teams={Immutable.fromJS(this.state.teams)}
             team={Immutable.fromJS(this.state.selectedTeam)}
